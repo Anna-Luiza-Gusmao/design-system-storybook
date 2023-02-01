70932a15559756701b7cf804ed148415ea9864b5
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-02-01 11:12:41 -0300

    AlterInputComponent

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,7 +1,7 @@
-import { ComponentElement } from 'react'
+import { ComponentProps } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
-export interface TextInputProps extends ComponentElement<typeof Input> {
+export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
 }
 
